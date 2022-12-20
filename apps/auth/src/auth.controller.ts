9033cfc4502356d36e0fdd2f7c8e425e3dbfadaf
Abderraouf <abdelraouffinch@gmail.com>
2022-12-20 23:11:19 +0100

    can create Queue but still not responding

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @MessagePattern({ cmd: 'get-user' })
+  async getUsers(@Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    channel.ack(originalMessage);
+    console.log(channel, originalMessage);
+
+    return { user: 'USER' };
   }
 }
